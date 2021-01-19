3fdc8f8445dc42b7630ca89cc0a143901167656f
ryosuke0829 <ryosuke0829@icloud.com>
2021-01-19 21:16:53 +0900

    2-2課題再提出
    引数の再設定

--- JavaStudy/2-2/src/study/Study.java
+++ JavaStudy/2-2/src/study/Study.java
@@ -12,9 +12,9 @@ class Account {
 
     // コンストラクタ
     public Account(String userName, int id, String password) {
-        this.userName = "hoge";
-        this.id = 123;
-        this.password = "abc123";
+        this.userName = userName;
+        this.id = id;
+        this.password = password;
     }
 
     // ① 以下のルールに従いアカウント情報（名前、ID、パスワード）を出力するメソッドを作成してください。
